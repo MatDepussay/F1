7d586c8f1d18d0728a55c54efe12b1a4761b609c
Mathias Depussay <matdepussay@gmail.com>
2026-02-23 10:24:43 +0100

    Lets go j'ai reussi a faire en sorte qu'on puisse suive le circuit alleluia

--- jeuxf1.py
+++ jeuxf1.py
@@ -14,6 +14,8 @@ class Car:
 class Track:
     width: int
     height: int
+    center_x: int
+    center_y: int
 
 
 
@@ -21,7 +23,7 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 
-track = Track(400, 300)
+track = Track(400, 300, center_x=400, center_y=300)
 car = Car(400, 200, 0, 0)
 
 running = True
@@ -41,11 +43,15 @@ while running:
         track.height,
     )
     
-
-    track_rect_int = pygame.Rect(0, 0, track.width // 2, track.height // 2)
+    rayon_x_ext = track.width // 2
+    rayon_y_ext = track.height // 2
+    center_x, center_y = track_rect_ext.center
     
+    track_rect_int = pygame.Rect(0, 0, rayon_x_ext, rayon_y_ext)
     track_rect_int.center = track_rect_ext.center
-    
+    rayon_x_int = rayon_x_ext // 2
+    rayon_y_int = rayon_y_ext // 2
+
     pygame.draw.ellipse(screen, (255, 0, 0), track_rect_ext)
     pygame.draw.ellipse(screen, (30, 30, 30), track_rect_int)
     
@@ -61,9 +67,21 @@ while running:
         car.angle -= 2
     if pygame.key.get_pressed()[pygame.K_RIGHT]:
         car.angle += 2
-
+        
+        
+    prev_x, prev_y = car.x, car.y
+            
     car.x += math.cos(math.radians(car.angle)) * 0.1*car.speed
     car.y += math.sin(math.radians(car.angle)) * 0.1*car.speed
+    
+    if ((car.x - center_x)/ rayon_x_ext)**2 + ((car.y - center_y)/ rayon_y_ext)**2 > 1:
+        car.speed = 0
+        car.x = prev_x
+        car.y = prev_y
+    if ((car.x - center_x)/ rayon_x_int)**2 + ((car.y - center_y)/ rayon_y_int)**2 < 1:
+        car.speed = 0
+        car.x = prev_x
+        car.y = prev_y
 
 
 pygame.quit()
